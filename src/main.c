85e9ab51d62350cc8b06c5661fbf99a521a7eae9
Sriram Ramasamy <sriramr083@gmail.com>
2025-11-07 17:23:58 +0530

    showing command not found error

--- src/main.c
+++ src/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+
+  // Captures the user's command in the "command" variable
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+
+  command[strcspn(command, "\n")] = '\0';
+
+  printf("%s: command not found", command);
 
   return 0;
 }
